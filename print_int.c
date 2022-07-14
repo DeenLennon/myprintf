ecc42818dbf54928f569d4c02f2861c022f7c03a
tovUsam <tovie.oba@gmail.com>
2022-07-13 17:50:59 -0700

    just pull please

--- print_int.c
+++ print_int.c
@@ -1,48 +1,42 @@
 #include "main.h"
-/**
-* print_int - function that prints an integer
-* @i: integer to print
-* Descriptions: prints digit with _putchar
-* Return: size the output text
-*/
-int print_int(va_list i)
-{
-	int len, powten, j, digit, n, count = 0, num;
 
-	n = va_arg(i, int);
-	if (n != 0)
+/**
+ * print_int - prints an integer
+ * @arguments: input string.
+ * @buf: buffer pointer.
+ * @ibuf: index for buffer pointer.
+ * Return: number of chars printed.
+ */
+int print_int(va_list arguments, char *buf, unsigned int ibuf)
+{
+	int int_input;
+	unsigned int int_in, int_temp, i, div, isneg;
+
+	int_input = va_arg(arguments, int);
+	isneg = 0;
+	if (int_input < 0)
 	{
-		if (n < 0)
-		{
-			_putchar('-');
-			count++;
-		}
-		num = n;
-		len = 0;
-		while (num != 0)
-		{
-			num /= 10;
-			len++;
-		}
-		powten = 1;
-		for (j = 1; j <= len - 1; j++)
-			powten *= 10;
-		for (j = 1; j <= len; j++)
-		{
-			digit = n / powten;
-			if (n < 0)
-				_putchar((digit * -1) + 48);
-			else
-				_putchar(digit + '0');
-			count++;
-			n -= digit * powten;
-			powten /= 10;
-		}
+		int_in = int_input * -1;
+		ibuf = handl_buf(buf, '-', ibuf);
+		isneg = 1;
 	}
 	else
 	{
-		_putchar('0');
-		return (1);
+		int_in = int_input;
 	}
-	return (count);
+
+	int_temp = int_in;
+	div = 1;
+
+	while (int_temp > 9)
+	{
+		div *= 10;
+		int_temp /= 10;
+	}
+
+	for (i = 0; div > 0; div /= 10, i++)
+	{
+		ibuf = handl_buf(buf, ((int_in / div) % 10) + '0', ibuf);
+	}
+	return (i + isneg);
 }
